4c92c55b302b3b8ec53ef2cb28ed8ec45da7bd76
Barkın Bayzin <barkin.bayzin@negzel.net>
2024-09-25 16:47:24 +0300

    Add project files.

---
+++ Newsletter.Api/Program.cs
@@ -0,0 +1,57 @@
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newsletter.Api.Database;
+using Newsletter.Api.Emails;
+using Newsletter.Api.Messages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IEmailService, EmailService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddMassTransit(busConfigurator =>
+{
+    busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+    busConfigurator.AddConsumers(typeof(Program).Assembly);
+
+    busConfigurator.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(new Uri(builder.Configuration.GetConnectionString("RabbitMq")!), hst =>
+        {
+            hst.Username("guest");
+            hst.Password("guest");
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+
+app.MapPost("/newsletters", async ([FromBody] string email, IBus bus) =>
+{
+    await bus.Publish(new SubscribeToNewsletter(email));
+
+    return Results.Accepted();
+});
+
+app.UseHttpsRedirection();
+
+app.Run();
